d3b9b2378ab9e9d92b9cef79556a72e5857bfb8d
Isabella Lopez <lopezisa@umich.edu>
2024-11-22 12:40:50 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -25,3 +25,43 @@ document.querySelector("#slower").addEventListener("click", function() {
 	video.playbackRate *= .90
 	console.log("Speed is ", video.playbackRate)
 });
+
+document.querySelector("#faster").addEventListener("click", function() {
+	console.log("speed up video")
+	video.playbackRate /= .90
+	console.log("Speed is ", video.playbackRate)
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	if (video.currentTime + 10 > video.duration) {
+		video.currentTime = 0
+	}
+	else {
+		video.currentTime += 10
+	}
+	console.log("Current time is ", video.currentTime)
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if (video.muted) {
+		video.muted = false
+		document.querySelector("#mute").innerHTML = "Mute"
+	}
+	else {
+		video.muted = true
+		document.querySelector("#mute").innerHTML = "Unmute"
+	}
+});
+
+document.querySelector("#slider").addEventListener("change", function() {
+	video.volume = document.querySelector("#slider").value/100
+	document.querySelector("#volume").innerHTML = video.volume*100 + "%"
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.classList.add("oldSchool")
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	video.classList.remove("oldSchool")
+});
